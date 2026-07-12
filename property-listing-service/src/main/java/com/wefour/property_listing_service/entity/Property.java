37edae570a8cbf95b2ed91614e324fb06d6135de
Kiran Chavan <kiran.chavan.ta@gmail.com>
2026-07-13 03:07:59 +0530

    Initial commit: Property Entity Add

--- property-listing-service/src/main/java/com/wefour/property_listing_service/entity/Property.java
+++ property-listing-service/src/main/java/com/wefour/property_listing_service/entity/Property.java
@@ -1,5 +1,28 @@
 package com.wefour.property_listing_service.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Property { 
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String address;
+    private String city;
+    private String description;
+    private Double price;
+    
 
 }
